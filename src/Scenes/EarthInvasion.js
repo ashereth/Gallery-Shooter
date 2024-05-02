607b618de6711a371edd9bff5c949fd5e6b95f4f
Asher <asherce21@gmail.com>
2024-05-02 09:47:48 -0700

    Update EarthInvasion.js

--- src/Scenes/EarthInvasion.js
+++ src/Scenes/EarthInvasion.js
@@ -9,28 +9,75 @@ class EarthInvasion extends Phaser.Scene {
 
         // array to hold all player bullets
         this.my.sprite.playerBullet = []; 
-        this.my.sprite.enemyBullet = [];  
-        
+        this.my.sprite.enemyBullet = [];         
     }
 
     preload() {
         this.load.setPath("./assets/");
         //load spritesheet
-        this.load.image("images/sheet");
+        this.load.atlasXML("spaceParts", "images/sheet.png", "images/sheet.xml");
         //load background
         this.load.image("background" ,"backgrounds/blue.png");
     }
 
     create() {
         let my = this.my;
+        //set how much each image should be scaled 
+        this.scale = .5;
+        //set player speed
+        this.playerSpeed = 6;
 
         //set background
-        my.sprite.background = this.add.sprite(200, 200, "background");
+        my.sprite.background = this.add.sprite(129, 129, "background");
+        my.sprite.background = this.add.sprite(385, 129, "background");
+        my.sprite.background = this.add.sprite(641, 129, "background");
+        my.sprite.background = this.add.sprite(850, 129, "background");
+        my.sprite.background = this.add.sprite(129, 385, "background");
+        my.sprite.background = this.add.sprite(129, 641, "background");
+        my.sprite.background = this.add.sprite(385, 385, "background");
+        my.sprite.background = this.add.sprite(385, 641, "background");
+        my.sprite.background = this.add.sprite(850, 385, "background");
+        my.sprite.background = this.add.sprite(850, 641, "background");
+        my.sprite.background = this.add.sprite(641, 385, "background");
+        my.sprite.background = this.add.sprite(641, 641, "background");
+
+        //make and scale the player ship
+        my.sprite.playerShip = this.add.sprite(game.config.width/2, 570, "spaceParts", "playerShip1_red.png");
+        my.sprite.playerShip.setScale(this.scale);
+
+        // Create keys
+        this.left = this.input.keyboard.addKey("A");
+        this.right = this.input.keyboard.addKey("D");
+        this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update() {
         let my = this.my;
+
+        // Moving left
+        if (this.left.isDown) {
+            // Check to make sure the sprite can actually move left
+            if (my.sprite.playerShip.x > (my.sprite.playerShip.displayWidth/2)+4) {
+                my.sprite.playerShip.x -= this.playerSpeed;
+            }
+        }
+
+        // Moving right
+        if (this.right.isDown) {
+            // Check to make sure the sprite can actually move right
+            if (my.sprite.playerShip.x < (game.config.width - (my.sprite.playerShip.displayWidth/2))-4) {
+                my.sprite.playerShip.x += this.playerSpeed;
+            }
+        }
+        // Check for bullet being fired
+        if (Phaser.Input.Keyboard.JustDown(this.space)) {
+            my.sprite.bullet = this.add.sprite(my.sprite.playerShip.x, my.sprite.playerShip.y-(my.sprite.playerShip.displayHeight/2), "spaceParts", "laserGreen07.png");
+            my.sprite.bullet.setScale(this.scale);
+            my.sprite.playerBullet.push(my.sprite.bullet);
+        }
     }
+
+    
 
     // A center-radius AABB collision check
     collides(a, b) {
